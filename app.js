61fe60adee285e9ad9d971899859f1a1b5223713
Michael Ethridge <maethridge@gmail.com>
2020-03-07 04:57:21 -0500

    Add compose.ejs, fix header.ejs bootstrap link

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+// An array of objs for each post a user makes. Each obj contains the title and post information.
+let posts = [];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -38,9 +41,15 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res){
-  const title = req.body.newTitle;
-  const entry = req.body.newEntry;
-  res.redirect("compose");
+
+  const post = {
+    title: req.body.newTitle,
+    content: req.body.newEntry
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
